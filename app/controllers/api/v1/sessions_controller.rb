0888d865fc0ade40e177d1285b2fa1c9f60d7d9c
korgiwannafly <duc.nguyentrung@vti.com.vn>
2021-07-27 15:30:27 +0700

    set up base jwt

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_email(user_params[:email])
+    
+    if @user&.authenticate(user_params[:password])
+      render json: {
+        token: JsonWebToken.encode(user_id: @user.id),
+        email: @user.email
+      }
+    else
+      head :unauthorized
+    end
+  end
+  
+  def destroy
+    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
